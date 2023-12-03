7ec83ac6045fac2d65992f2a6c3c6893499bbc4b
31783 <dongjinwang040204@gmail.com>
2023-12-03 17:52:18 +0800

    Answer

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -53,6 +53,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      */
     @Override
     public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
         return rb[first];
     }
 
