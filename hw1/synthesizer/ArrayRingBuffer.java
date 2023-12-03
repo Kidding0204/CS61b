b218b8b9f6a6bb07e5fd601be3bed1f6aeb05b85
31783 <dongjinwang040204@gmail.com>
2023-12-03 17:43:37 +0800

    Answer

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,78 @@
+package synthesizer;
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        rb = (T[]) new Object[capacity];
+        this.capacity = capacity;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public void enqueue(T x) {
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        last = (last + 1) % capacity;
+        fillCount++;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T returnItem = rb[first];
+        first = (first + 1) % capacity;
+        fillCount--;
+        return returnItem;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    @Override
+    public T peek() {
+        return rb[first];
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new BufferIterator<T>();
+    }
+    private class BufferIterator<T> implements Iterator<T> {
+        private int iteratorFirst = first;
+        @Override
+        public boolean hasNext() {
+            return iteratorFirst != last;
+        }
+        @Override
+        public T next() {
+            T result = (T) rb[iteratorFirst];
+            iteratorFirst = (iteratorFirst + 1) % capacity;
+            return result;
+        }
+    }
+}
+
