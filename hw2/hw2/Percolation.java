2ee71af1d148662778a82724fd80f30cd59c0aeb
31783 <dongjinwang040204@gmail.com>
2023-12-19 13:38:22 +0800

    1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,34 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    boolean[][] sites;
+    boolean[][] waters;
+    int openNum;
+    WeightedQuickUnionUF sets;
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N can't equal or smaller than 0!");
+        }
+        sites = new boolean[N][N];
+        waters = new boolean[N][N];
+        for (boolean top : waters[N-1]) {
+            top = true;
+        }
+        openNum = 0;
+        sets = new WeightedQuickUnionUF(N * N);
+    }
 
+    public void open(int row, int col) {
+        sites[row][col] = true;
+        openNum++;
+    }
+    public boolean isOpen(int row, int col) {
+        return sites[row][col];
+    }
+    public int numberOfOpenSites() {
+        return openNum;
+    }
+    public static void main(String[] args) {
+
+    }
 }
