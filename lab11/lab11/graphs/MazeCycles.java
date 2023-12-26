7f8844685a52fc78a99ad714577dea38b5ac3c47
31783 <dongjinwang040204@gmail.com>
2023-12-26 10:02:55 +0800

    CycleFound completed

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,45 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean cycleFound = false;
+    private int cross = -1;
+    private Maze maze;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        cycleFound(0, 0);
     }
 
     // Helper methods go here
+    private void cycleFound(int v, int pre) {
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (w == pre) {
+                continue;
+            }
+            if (marked[w]) {
+                cross = w;
+                cycleFound = true;
+                edgeTo[w] = v;
+                return;
+            } else {
+                cycleFound(w, v);
+                edgeTo[w] = v;
+                break;
+            }
+        }
+
+        if (v == cross) {
+            announce();
+        }
+    }
 }
 
