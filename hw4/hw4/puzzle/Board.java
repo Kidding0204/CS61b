41a079a15c5c19d44fa87893147764bd9cb409c3
31783 <dongjinwang040204@gmail.com>
2023-12-29 13:12:02 +0800

    initial board

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,39 @@
 package hw4.puzzle;
 
-public class Board {
+public class Board implements WorldState{
+    public Board(int[][] tiles) {
+
+    }
+
+    @Override
+    public int estimatedDistanceToGoal() {
+        return 0;
+    }
+    @Override
+    public Iterable<WorldState> neighbors() {
+        return null;
+    }
+
+    public int tileAt(int i, int j) {
+        return 0;
+    }
+    public int size() {
+        return 0;
+    }
+    public int hamming() {
+        return 0;
+    }
+    public int manhattan() {
+        return 0;
+    }
+    @Override
+    public boolean equals(Object y) {
+        return false;
+    }
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    /*public String toString() {
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +45,5 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
-
+    }
 }
