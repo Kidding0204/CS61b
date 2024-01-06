8c2e6921f2c53681414094aa3c481be9fb3f0730
31783 <dongjinwang040204@gmail.com>
2024-01-06 23:01:01 +0800

    HuffmanEncoder completed

--- hw7/HuffmanEncoder.java
+++ hw7/HuffmanEncoder.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class HuffmanEncoder {
     public static Map<Character, Integer> buildFrequencyTable(char[] inputSymbols) {
@@ -20,6 +19,14 @@ public class HuffmanEncoder {
         char[] file8Bit = FileUtils.readFile(args[0]);
         Map<Character, Integer> freqTable = buildFrequencyTable(file8Bit);
         BinaryTrie trie = new BinaryTrie(freqTable);
-
+        ObjectWriter ow = new ObjectWriter(STR."\{args[0]}.huf");
+        ow.writeObject(trie);
+        Map<Character, BitSequence> table = trie.buildLookupTable();
+        List<BitSequence> bitSequences = new ArrayList<>();
+        for (char character : file8Bit) {
+            bitSequences.add(table.get(character));
+        }
+        BitSequence all = BitSequence.assemble(bitSequences);
+        ow.writeObject(all);
     }
 }
